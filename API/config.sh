23b770a5daed9b400d8ce22304b2378f4332b78e
Siwa12100 <jean.marcillac12@gmail.com>
2025-02-10 15:35:56 +0000

    ajout docker

---
+++ API/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Configuration du projet
+IMAGE_NAME="projet-image-api:latest"
+CONTAINER_NAME="projet-image-api"
+PORT="21090"
+
+export IMAGE_NAME
+export CONTAINER_NAME
+export PORT
